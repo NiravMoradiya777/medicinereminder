536ed5f1ec42e1fea0816835b8e712b2a7b68351
NiravMoradiya <niravmoradiya555@gmail.com>
2023-12-16 06:59:37 -0500

    Remove DevTool

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { disableRectDevTools } from '@fvilers/disable-react-devtools';
-
-if(process.env.NODE_ENV === 'production') disableRectDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
